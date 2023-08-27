df417e690e800698066c6c113d01c78dead73f01
BRUNO <brunomacielm@gmail.com>
2023-08-26 21:22:19 -0300

    Automate the evaluation process in the code/run_car_racing.py

--- code/run_car_racing.py
+++ code/run_car_racing.py
@@ -1,7 +1,10 @@
+import os
 import cv2
 import torch
 import wandb
 import numpy as np
+from utils import Params
+import matplotlib.pyplot as plt
 from cart_racing_v2 import CarRacing
 from data_preprocessing import DataHandler
 from record_observations import RecordObservations
@@ -9,40 +12,58 @@ from models import Model_Cond_Diffusion, Model_cnn_mlp
 
 
 class Tester(RecordObservations):
-    def __init__(self, model, env, render=True, device="cpu"):
+    def __init__(self, model, env, render=True, device="cpu", name="version"):
         super(Tester).__init__()
         self.model = model
         self.env = env
+        self.name = name
         self.render = render
         self.device = device
 
     def run(self, run_wandb, name=''):
+        self.name = name
         if run_wandb:
             self.config_wandb(project_name="car-racing-diffuser-bc-v2", name=name)
-        obs, _ = self.env.reset()
-        reward = 0
-        counter=0
-        done = False
-        truncated = False
-        while counter < 1000:
-            self.model.eval()
-            obs_tensor = self.preprocess_obs(obs)
-            torch.from_numpy(obs_tensor).float().to(self.device).shape
-            obs_tensor = (
-                torch.Tensor(obs_tensor).type(torch.FloatTensor).to(self.device)
-                )
-            action = self.model.sample(obs_tensor).to(self.device)
-            obs, new_reward, done, _, truncated = self.env.step(action.detach().cpu().numpy()[0])
-            self.array_to_img(obs, action, frame=counter)
-            reward += new_reward
-            counter += 1
-            print(reward)
-            if done or truncated: 
-                break
-            if run_wandb:
-                wandb.log({"reward": reward})
-        if run_wandb: wandb.finish()
+        test_epoch = 0
+        reward_list = []          
+        while test_epoch < 30:
+            obs, _ = self.env.reset()
+            reward = 0
+            counter=0
+            done = False
+            truncated = False
+            while counter < 1000:
+                self.model.eval()
+                obs_tensor = self.preprocess_obs(obs)
+                torch.from_numpy(obs_tensor).float().to(self.device).shape
+                obs_tensor = (
+                    torch.Tensor(obs_tensor).type(torch.FloatTensor).to(self.device)
+                    )
+                action = self.model.sample(obs_tensor).to(self.device)
+                obs, new_reward, done, _, truncated = self.env.step(action.detach().cpu().numpy()[0])
+                self.array_to_img(obs, action, frame=counter)
+                reward += new_reward
+                counter += 1
+                print(reward)
+                if done or truncated: 
+                    break
+                if run_wandb:
+                    wandb.log({"reward": reward})
+            if run_wandb: wandb.finish()
+            test_epoch += 1
+            reward_list.append(reward)
+        self.scatter_plot_reward(reward_list)
 
+    def scatter_plot_reward(self, reward_list):
+        plt.subplot()
+        plt.scatter(range(len(reward_list)), reward_list)
+        plt.title(f"Reward Scatter {self.name}")
+        plt.ylabel("Reward")
+        plt.xlabel("Episode")
+        plt.grid()
+        path = "experiments/"+self.name+"/"
+        os.makedirs(path, exist_ok=True)
+        plt.savefig(path+self.name+"_scatter.png")
 
     def config_wandb(self, project_name, name):
         config={}
@@ -62,36 +83,49 @@ def get_dim(x, y):
     pass
 
 if __name__ == '__main__':
-    n_epoch = 40
-    lrate = 1e-4
-    device = "cpu"
-    n_hidden = 128
-    batch_size = 32
-    n_T = 50
-    net_type = "transformer"
-    drop_prob = 0.0
-    extra_diffusion_steps = 16
-    x_shape = (96, 96, 4)
-    y_dim = 3
+    versions = [f for f in os.listdir("experiments") if ('version' in f and '.pkl' not in f)]
+    for version in sorted(versions):
+        params = Params("experiments/" + version + "/params.json")
 
-    env = CarRacing(render_mode="human") 
-    nn_model = Model_cnn_mlp(
-        x_shape, n_hidden, y_dim, embed_dim=128, net_type=net_type
-    ).to(device)
+        n_epoch = params.n_epoch
+        lrate = params.lrate
+        device = "cpu"
+        n_hidden = params.n_hidden
+        batch_size = params.batch_size
+        n_T = params.n_T
+        net_type = params.net_type
+        drop_prob = params.drop_prob
+        extra_diffusion_steps = params.extra_diffusion_steps
+        embed_dim = params.embed_dim
+        x_shape = (96, 96, 4)
+        y_dim = 3
 
-    model = Model_Cond_Diffusion(
-        nn_model,
-        betas=(1e-4, 0.02),
-        n_T=n_T,
-        device=device,
-        x_dim=x_shape,
-        y_dim=y_dim,
-        drop_prob=drop_prob,
-    guide_w=0.0,)
+        env = CarRacing(render_mode="rgb_array") 
+        nn_model = Model_cnn_mlp(
+            x_shape, n_hidden, y_dim, embed_dim=embed_dim, net_type=net_type
+        ).to(device)
 
-    # model.load_state_dict(torch.load("model_casa2.pkl"))
-    model.load_state_dict(torch.load("model_novo_bc.pkl"))
+        model = Model_Cond_Diffusion(
+            nn_model,
+            betas=(1e-4, 0.02),
+            n_T=n_T,
+            device=device,
+            x_dim=x_shape,
+            y_dim=y_dim,
+            drop_prob=drop_prob,
+            guide_w=0.0,)
 
-    stop = 1
-    tester = Tester(model, env, render=True)
-    tester.run(run_wandb=False)
+        # model.load_state_dict(torch.load("model_casa2.pkl"))
+        model.load_state_dict(torch.load("experiments/"+version+"/"+version+".pkl",
+                            map_location=torch.device('cpu')))
+
+        stop = 1
+        tester = Tester(model, env, render=True)
+        try:
+            tester.run(run_wandb=False, name=version)
+        except Exception as exception:
+            print("---------------------------------------------------")
+            print(f"The {version} couldn't be trained due to ")
+            print(f'{exception}')
+            print("---------------------------------------------------")
+            continue
