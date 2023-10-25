e39a86a75879bb7893d3cae017f9bd7d41ba0894
BRUNO <brunomacielm@gmail.com>
2023-10-25 18:54:41 -0300

    Modify the reading of models in code/run_car_racing.py

--- code/run_car_racing.py
+++ code/run_car_racing.py
@@ -52,7 +52,7 @@ class Tester(RecordObservations):
             self.config_wandb(project_name="car-racing-diffuser-bc-human-eval", name=name)
         episode = 0
         reward_list = []        
-        while episode < 1:
+        while episode < 20:
             # np.random.seed(40) # 1
             obs, _ = self.env.reset()
             reward = 0
@@ -129,15 +129,18 @@ class Tester(RecordObservations):
 
 
 if __name__ == '__main__':
-    versions = ["version_3", "version_4"]
+    versions_path = "/Users/brunomaciel/Desktop/human-models/"
+    version_numbers = [0, 1, 2, 3, 8, 9, 10, 11]
+    # versions = ["version_3", "version_4"]
     gains = [4, 4.5, 5.5, 3.5, 3, 1, 5.5]
     for gain in gains:
-        for version in sorted(versions):
-            params = Params("experiments/" + version + "/params.json")
+        for version in sorted(version_numbers):
+            name = versions_path + "version_" + str(version)
+            params = Params(name + "/params.json")
 
             n_epoch = params.n_epoch
             lrate = params.lrate
-            device = "cpu"
+            device = "mps"
             n_hidden = params.n_hidden
             batch_size = params.batch_size
             n_T = params.n_T
@@ -148,7 +151,7 @@ if __name__ == '__main__':
             x_shape = (96, 96, 4)
             y_dim = 3
 
-            env = CarRacing(render_mode="human") 
+            env = CarRacing(render_mode="rgb-array") 
             nn_model = Model_cnn_mlp(
                 x_shape, n_hidden, y_dim, embed_dim=embed_dim, net_type=net_type
             ).to(device)
@@ -164,13 +167,15 @@ if __name__ == '__main__':
                 guide_w=0.0,)
 
             # model.load_state_dict(torch.load("model_casa2.pkl"))
-            model.load_state_dict(torch.load("experiments/"+version+"/"+version+".pkl",
+            model.load_state_dict(torch.load(name + "_model_best_reward.pkl",
                                 map_location=torch.device('mps')))
 
             stop = 1
             tester = Tester(model, env, render=True, device=device)
             try:
-                tester.run(run_wandb=False, name=version, gain=gain)
+                tester.run(run_wandb=False,
+                           name="version_" + str(version),
+                           gain=gain)
             except Exception as exception:
                 print("---------------------------------------------------")
                 print(f"The {version} couldn't be trained due to ")
